45f25e9f6deeb7dffaa246e400f1ddbf3e9b66ed
zgw94 <zgw@suadnim.com>
2022-07-20 18:19:53 +0800

    test

---
+++ test.java
@@ -0,0 +1,13 @@
+public class test {
+    public static void main(String args[]){
+        System.out.println("阶乘作业");
+        int x = 1;
+        int y = 1;
+        while (y <= 10){
+            x = x * y;
+            y ++;
+        }
+        System.out.println(x);
+        //System.out.println(x);
+    }
+}
